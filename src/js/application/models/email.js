d5c7cb59638c393f0ffcdb15fc2a81433039b6bf
Matthew Clark <matthewclarkj@gmail.com>
2014-09-15 10:28:51 -0700

    Email validation

--- src/js/application/models/email.js
+++ src/js/application/models/email.js
@@ -8,11 +8,13 @@ define([
                 return 'http://localhost/interact/email';
             },
             validateAddress: function () {
-                // TODO: Validate email
-                if (this.get('address')) {
-                    return true;
-                } else {
+                var x = this.get('address');
+                var atpos = x.indexOf("@");
+                var dotpos = x.lastIndexOf(".");
+                if (atpos< 1 || dotpos<atpos+2 || dotpos+2>=x.length) {
                     return false;
+                } else {
+                    return true;
                 }
             }
         });
